fbd7120fc03fe03d21005feb4f8d6d005ac744e9
Miglio40 <maacevedo054@soy.sena.edu.co>
2026-07-02 09:43:59 -0500

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <--- Esta es la línea que soluciona el pantallazo blanco
   plugins: [react()],
   server: {
     proxy: {
