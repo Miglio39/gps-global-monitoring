65953c007daf4a1caf6eb328ddbc4d1b08e58e1a
Miglio40 <maacevedo054@soy.sena.edu.co>
2026-07-10 15:56:07 -0500

    voitecontrol

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // <--- Esta es la línea que soluciona el pantallazo blanco3
+  base: '/', // <--- ¡QUÍTALE EL PUNTO! Debe quedar solo la barra
   plugins: [react()],
   server: {
     proxy: {
